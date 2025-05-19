c11c39945157a5dce47f907ea4c3033d4759d7ed
gioiascari <gioia.ascari96@gmail.com>
2025-05-20 01:10:39 +0200

    fix bugs

--- src/Music.java
+++ src/Music.java
@@ -1,14 +1,42 @@
 import javax.sound.*;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.*;
 import java.io.File;
+import java.io.IOException;
+
 public class Music {
     private Clip clip;
 
-    public boolean load(String filePath){
-        File fa = new File(filePath);
+    public static void load(String filePath) throws Exception {
+        new Thread(() ->{
+            try {
+            File fa = new File(filePath);
+            AudioInputStream as = AudioSystem.getAudioInputStream(new File(filePath));
+            Clip clip = AudioSystem.getClip();
+            clip.open(as);
+            clip.start();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
 
+            }
+        }).start();
+    }
+
+    public void play() {
+        if(clip!=null){
+            clip.start();
+        }else{
+            System.out.println("No clip loaded");
+        }
+    }
+    public void stop() {
+        if(clip!=null && clip.isRunning()){
+            clip.stop();
+        }
+    }
+    public void close() {
+        if(clip!=null){
+            clip.close();
+        }
     }
 
 }
