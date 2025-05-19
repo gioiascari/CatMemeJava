86da28c3ffb85d5c283886e49b6008bef14b496b
gioiascari <gioia.ascari96@gmail.com>
2025-05-20 00:44:54 +0200

    init music classs

---
+++ src/Music.java
@@ -0,0 +1,14 @@
+import javax.sound.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+public class Music {
+    private Clip clip;
+
+    public boolean load(String filePath){
+        File fa = new File(filePath);
+
+    }
+
+}
