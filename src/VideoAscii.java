134cfe08279785b35911394b44c15d6532d436a2
gioiascari <gioia.ascari96@gmail.com>
2025-05-20 01:16:22 +0200

    init ascii

--- src/VideoAscii.java
+++ src/VideoAscii.java
@@ -1,2 +1,10 @@
 public class VideoAscii {
+    public static final String ASCII= "OIIA";
+    public static final int width = 90;
+    public static final int height = 45;
+    public static final double contrast=1.7;
+    public static final int btd = 5;//luminosità
+    public static void main(String[] args) {
+
+    }
 }
