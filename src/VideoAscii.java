b78578fc37c29592caca3ae66377aa5c267d706a
gioiascari <gioia.ascari96@gmail.com>
2025-05-24 22:06:39 +0200

    init VideoAscii.java in maniera più agile

--- src/VideoAscii.java
+++ src/VideoAscii.java
@@ -1,27 +1,18 @@
 import java.io.File;
 
 public class VideoAscii {
-    public static final String ASCII= "OIIA";
-    public static final int width = 90;
-    public static final int height = 45;
-    public static final double contrast=1.7;
-    public static final int btd = 5;//luminosità
+    private final int width;
+    private final int height;
+    private final double alpha;
+    private final double beta;
+    private final double gamma;
+    private final String micio = "OIA";
 
-    private static void loadOpenCV(){
-        String path = "opencv/";
-        String os = System.getProperty("os.name").toLowerCase();
-        if(os.contains("win")){if(os.contains("win)")){
-            System.load(new File(path + "opencv_java460.dll").getAbsolutePath());
-        }
-
-        }
-    }
-    private static boolean isGreen(int r, int g, int b){
-        int dr = r;
-        int dg = g;
-        int db = b;
-        double dist=Math.sqrt(dr*dr+dg*dg+db*db);
-        return dist < 100;
-
+    public VideoAscii(int width, int height, double alpha, double beta, double gamma) {
+        this.width = width;
+        this.height = height;
+        this.alpha = alpha;
+        this.beta = beta;
+        this.gamma = gamma;
     }
 }
