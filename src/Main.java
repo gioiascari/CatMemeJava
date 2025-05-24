7553087b5f17c9ea02a12bf40fd11b53463d9aa7
gioiascari <gioia.ascari96@gmail.com>
2025-05-25 01:19:02 +0200

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import Ascii.AsciiVideo;
+import Ask.AskMe;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception {
+        String audio = "audio.wav";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Music.playASY(audio);
+        AsciiVideo video = new AsciiVideo(90,45,1.5,5,100);
+
+        AskMe askMe = new AskMe(video);
+        askMe.helloHuman();
+
     }
 }
